f7fac3ddd2c85e803944f9d0c48c2b2d595ee22a
Nasrullah <nasrullah01n@gmail.com>
2024-01-14 20:23:44 +0800

    Removed delay from skills on viewport animation

--- src/components/sections/skills/index.tsx
+++ src/components/sections/skills/index.tsx
@@ -12,38 +12,35 @@ export const Skills = () => {
       <h1 className="mb-8 w-full text-center text-3xl font-bold capitalize">
         My Skills
       </h1>
-      {skills.map((skillSection, sectionIndex) => (
+      {skills.map(({ section, frameworks, path }) => (
         <AnimateIn
           scroll
-          transition={{
-            delay: sectionIndex * 0.05,
-          }}
           viewport={{
             once: true,
             amount: 0.1,
           }}
           className="mx-auto mb-8 flex flex-col items-center"
-          key={skillSection.section}
+          key={section}
         >
           <h1 className="it mb-2 text-xl font-bold capitalize text-muted-foreground">
-            {skillSection.section}
+            {section}
           </h1>
           <div className="flex flex-wrap justify-center gap-4">
-            {skillSection.frameworks.map(skill => (
+            {frameworks.map(({ name, url, icon }) => (
               <a
-                key={skill.name}
-                href={skill.url}
+                key={name}
+                href={url}
                 target="_blank"
                 className="flex cursor-pointer items-center gap-2 rounded-full border border-border bg-background px-4 py-2 text-sm font-bold transition hover:scale-110 hover:bg-foreground/5 dark:bg-muted dark:hover:bg-muted/70"
               >
                 <Image
-                  src={`/icons/${skillSection.path}/${skill.icon}.svg`}
+                  src={`/icons/${path}/${icon}.svg`}
                   className="rounded-full bg-background p-1 dark:bg-foreground"
                   width={32}
                   height={32}
                   alt=""
                 />
-                {skill.name}
+                {name}
               </a>
             ))}
           </div>
