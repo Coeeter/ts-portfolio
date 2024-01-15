12a3079917a9a034c27fcf5e56103cadc217c9fe
Nasrullah <nasrullah01n@gmail.com>
2024-01-16 00:04:17 +0800

    Fixed footer bugs for error and not found pages

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/components/ui/button';
 
 const ErrorPage = ({ error, reset }: { error: Error; reset: () => void }) => {
   return (
-    <div className="mx-auto flex h-full w-full max-w-sm flex-col justify-center gap-6">
+    <div className="mx-auto flex h-screen w-full max-w-sm flex-col justify-center gap-6">
       <h1 className="text-3xl font-bold">Something went wrong</h1>
       {process.env.NODE_ENV === 'development' ? (
         <pre className="w-full overflow-hidden text-wrap text-muted-foreground">
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const NotFound = () => {
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center">
+    <div className="flex h-screen w-full flex-col items-center justify-center">
       <div className="flex max-w-md flex-col gap-3">
         <h1 className="font-montserrat text-3xl font-bold">404 Not Found</h1>
         <p className="mb-2 text-muted-foreground">
